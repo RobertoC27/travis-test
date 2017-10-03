6452e42b2a3ee436fd748761e157c8f067429ea9
Unknown <chi13027@uvg.edu.gt>
2017-10-03 12:52:03 -0600

    alkmc

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ import unittest
 class simpleTest (unittest.TestCase):
 	def setUp(self):
 		print 'hehe'
+		self.assertTrue(1,1)
 	def tearDown(self):
 		print 'pasado con exito'
 	def test_extra(self):
